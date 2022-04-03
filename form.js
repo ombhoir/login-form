1b6d6ca0ebf308e19bf048b957cd598dc8a8664e
ombhoir <79741241+ombhoir@users.noreply.github.com>
2022-04-03 15:49:02 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,9 @@
+var scriptURL="https://script.google.com/macros/s/AKfycbwrCKWJvIWr4LI5Gk_GKZvDDbz8EV83dFc_4oQyMngGZEkZtGywtNsHQLx4CWkF4gEq/exec"
+var form = document.forms['google-sheet']
+
+form.addEventListener('submit', e=>{
+e.preventDefault()
+fetch(scriptURL,{method: 'POST', body: new FormData(form)})
+.then(response => document.getElementById('form_alert').innerHTML = "Data has stored")
+.catch(error => document.getElementById('form_alert').innerHTML = "Data has not stored")
+})
